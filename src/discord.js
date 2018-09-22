7548f704353520c006d95c38199ea22aa09f67b0
Max <41818950+donutcake1@users.noreply.github.com>
2018-09-22 15:56:51 +0300

    hlep me

--- src/discord.js
+++ src/discord.js
@@ -1,9 +1,9 @@
 const Eris = require('eris');
 const CatLoggr = require('cat-loggr');
 const catprefix = "cat!";
+const config = require('./config.json');
 
-var bot = new Eris(<place_your_token_there>);
-
+var bot = new Eris(config.config.bot.token);
 const loggr = new CatLoggr({
     levels: [
         { name: 'info', color: CatLoggr._chalk.red.bgBlack },
@@ -26,19 +26,23 @@ bot.on("messageCreate", (msg) => {
 
 bot.on("messageCreate", (msg) => {
     if(msg.content === catprefix + "reload"){
-        if(msg.author.id != "241612639560531968"){
+        if(msg.author.id != config.config.donut_id){
             bot.createMessage(msg.channel.id, "You look pretty suspicious to reboot me... :thinking:");
             console.warn(msg.author.id + "... They think they can reboot me...");
         } else {
             console.reload("Got Donut rights! Unloading...");
             bot.disconnect();
+            console.reload("Unloaded, reconnecting...");
+            bot.connect();
+            bot.on("connect", (id) => {
+                console.reload("Shard " + id + " reloaded successfully!");
+            });
         }
     }
 });
 
-bot.on("disconnect", () => {
-    console.reload("Disconnected! Reconnecting...");
-    bot.connect();
+bot.on("warn", (message, id) => {
+    console.warn("On shard " + id + "is happening something: " + message);
 })
 
 bot.connect();
